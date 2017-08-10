c8dab658a3b3a15ab0d24bd7c64e070f5ae26515
Mark A. Miller <turbomam@phaseolus.biz>
2017-08-10 13:46:24 -0400

    refactored imports

--- helloEclipse/src/main/scala/RDF4JFilterTest.scala
+++ helloEclipse/src/main/scala/RDF4JFilterTest.scala
@@ -1,30 +1,9 @@
-import org.eclipse.rdf4j.query.QueryLanguage
-import org.eclipse.rdf4j.repository.http.HTTPRepository
-import org.eclipse.rdf4j.repository.manager.RemoteRepositoryManager._
-
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
-
-import org.eclipse.rdf4j.query.QueryResults
-import org.eclipse.rdf4j.query.QueryResults._
-
-import scala.collection.JavaConversions._
-
-import org.eclipse.rdf4j.model
-import org.eclipse.rdf4j.model._
-import org.eclipse.rdf4j.model.Statement
-
-import org.eclipse.rdf4j.model
-import org.eclipse.rdf4j.model._
-import org.eclipse.rdf4j.model.Literal
-
-import org.eclipse.rdf4j.model.vocabulary.XMLSchema
+import java.util.stream.Collectors
 
 import org.eclipse.rdf4j.model.util.Models
-
-import java.util.stream
-import java.util.stream._
-import java.util.stream.Collector
+import org.eclipse.rdf4j.model.vocabulary.XMLSchema
+import org.eclipse.rdf4j.query.QueryResults
+import org.eclipse.rdf4j.repository.http.HTTPRepository
 
 object RDF4JFilterTest {
 
@@ -41,9 +20,6 @@ object RDF4JFilterTest {
     val DumpTruck = f.createIRI("http://example.com/dumpTruck")
     val Weight = f.createIRI("http://example.com/weight")
 
-    // cRud: read, rdf4j getStatements solution, analogous to sparql select
-    var gsResult = con.getStatements(DumpTruck, Weight, null)
-    
     val m = QueryResults.asModel(con.getStatements(DumpTruck, Weight, null))
     val intValuesStream = 
       Models.objectLiterals(m).stream()
@@ -53,15 +29,9 @@ object RDF4JFilterTest {
     
     System.out.println(intValues.toString())
 
-    gsResult.close()
-
     MyRepo.shutDown
 
     con.close
-
-    //    RepoMan.shutDown()
-
-    println("\ndone")
 
   }
 }
