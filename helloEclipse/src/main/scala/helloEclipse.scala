85d2f126242cac57c365927d7844cee61ffb811a
Mark A. Miller <turbomam@phaseolus.biz>
2017-08-10 10:51:23 -0400

    importing rdf4j

--- helloEclipse/src/main/scala/helloEclipse.scala
+++ helloEclipse/src/main/scala/helloEclipse.scala
@@ -1,6 +1,11 @@
+import org.eclipse.rdf4j.model.ValueFactory
+import org.eclipse.rdf4j.model.impl.SimpleValueFactory
 
 
 object helloEclipse {
       def main(args: Array[String]) =
         println("Hello World!")
+        
+        val factory = SimpleValueFactory.getInstance()
+      
 }
